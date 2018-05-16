e5a43dcf77b252a4d7da93eb13bf2ac5577809c7
Jamie Williams <jamieinfinity@mac.com>
2018-05-15 21:22:01 -0500

    closed #3: added act number and title to act cells - Jamie

--- client/src/js/movie_map.js
+++ client/src/js/movie_map.js
@@ -2,20 +2,7 @@ import {select} from "d3-selection";
 import {json, tsv} from "d3-request";
 
 
-/*
-TODO:
-- write ETL code to process movie data:
-    - into an array of (max_num_shots + 2) x num_scenes = (51+2) x 157
-    - the first column contains act info (e.g. title): only 4 rows have actual data; the rest are nulls (or some such)
-    - the second column contains scene info (e.g. title): every row has data
-    - the remaining columns contain info for each shot, up to a max of 51 shots. If there are less the 51 shots, fill
-    in the empty columns with nulls (or some such). Those columns will appear empty.
-    - eventually, you can add data like scene length, shot length, etc
-    - eventually you can add tooltips that display full image(s)
-    - eventually you can make selecting a row cause that row to expand:
-        - increase in size (MED image)
-        - add another row to display the dialog
-*/
+const romanNums = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V'};
 
 function groupByScene(prev, cur) {
     let result = prev, key = cur.scene;
@@ -27,21 +14,37 @@ function groupByScene(prev, cur) {
     return result;
 }
 
+function makeTabRowData(d) {
+    return '<tr style="color:#fff" class="tr_data">' +
+        '<td class="td_data"' + '><b>' + d.number + '</b></td>' +
+        '<td class="td_data"' + '>' + '0:00' + '</td>' +
+        '<td class="td_data" align="right"' + '>' + '20 mins' + '</td>' +
+        '</tr>';
+}
+
+function makeTabRowTitle(title) {
+    return '<tr style="color:#fff" class="tr_title">' +
+        '<td colspan="4" class="td_title"' + '>' + title + '</td>' +
+        '</tr>';
+}
+
 function buildApp(domElementID) {
 
-    let actTitles = {}, sceneTitles = {}, shotGrid = [], groupedShots,
-        numScenes, maxNumShots;
+    let actTitles = {}, actNumbers = {}, sceneTitles = {}, sceneNumbers = {}, shotGrid = [], groupedShots,
+        numActs, numScenes, maxNumShots, prevAct, actAdjustment;
 
     tsv('./film_data/acts.tsv', function (error, dataActs) {
-        dataActs.forEach(function (d) {
+        dataActs.forEach(function (d, i) {
             actTitles[d.act] = d.title;
+            actNumbers[d.act] = romanNums[i+1];
         });
+        numActs = Object.keys(actTitles).length;
 
         tsv('./film_data/scenes.tsv', function (error, dataScenes) {
-            dataScenes.forEach(function (d) {
+            dataScenes.forEach(function (d, i) {
                 sceneTitles[d.scene] = d.title;
+                sceneNumbers[d.scene] = i+1;
             });
-
             numScenes = Object.keys(sceneTitles).length;
 
             json("./film_data/shots.json", function (dataShots) {
@@ -56,41 +59,59 @@ function buildApp(domElementID) {
                     return Number(d.length);
                 }));
 
-                for (let i = 0; i < numScenes * (maxNumShots+2); i++) {
+                for (let i = 0; i < (numActs + numScenes) * (maxNumShots + 1); i++) {
                     shotGrid.push({
                         "class": "blank"
                     });
                 }
 
+                prevAct = "";
+                actAdjustment = 0;
                 for (let i = 0; i < numScenes; i++) {
                     let shots = groupedShots[i],
                         shotInfo = shots[0],
-                        shotCell = shotGrid[i*(maxNumShots+2)];
-                    shotCell["class"] = "act";
-                    shotCell["title"] = actTitles[shotInfo.act];
-                    shotCell = shotGrid[i*(maxNumShots+2)+1];
+                        shotCell;
+                    if (shotInfo.act !== prevAct) {
+                        prevAct = shotInfo.act;
+                        shotCell = shotGrid[(i + actAdjustment) * (maxNumShots + 1)];
+                        shotCell["class"] = "act";
+                        shotCell["title"] = actTitles[shotInfo.act];
+                        shotCell["number"] = actNumbers[shotInfo.act];
+                        actAdjustment += 1;
+                    }
+                    shotCell = shotGrid[(i + actAdjustment) * (maxNumShots + 1)];
                     shotCell["class"] = "scene";
                     shotCell["title"] = sceneTitles[shotInfo.scene];
-                    for (let j = 0; j<shots.length; j++) {
-                        shotInfo=shots[j];
-                        shotCell = shotGrid[i*(maxNumShots+2) + j + 2];
+                    shotCell["number"] = sceneNumbers[shotInfo.scene];
+                    for (let j = 0; j < shots.length; j++) {
+                        shotInfo = shots[j];
+                        shotCell = shotGrid[(i + actAdjustment) * (maxNumShots + 1) + j + 1];
                         shotCell["class"] = "shot";
                         shotCell["imageId"] = shotInfo.frames[0];
                     }
                 }
 
                 let grid = select(domElementID).append("div").attr("id", "grid").attr("class", "grid"),
-                    cells = grid.selectAll("div").data(shotGrid).enter().append("div").
-                    attr("class", function(d) {
+                    cells = grid.selectAll("div").data(shotGrid).enter().append("div").attr("class", function (d) {
                         return "cell " + d.class;
                     });
 
-                grid.style("grid-template-columns", "180px 270px repeat(" + maxNumShots + ", 90px)")
-                    .style("grid-template-rows", "repeat(" + numScenes + ", 45px)");
+                grid.style("grid-template-columns", "270px repeat(" + maxNumShots + ", 90px)")
+                    .style("grid-template-rows", "repeat(" + (numActs + numScenes) + ", 45px)");
 
                 cells.style("background-image", function (d) {
-                    return (d.class==="shot") ? 'url("./film_data/shot_images/shotImageSM_' + (d.imageId) + '.jpeg")' : '';
+                    return (d.class === "shot") ? 'url("./film_data/shot_images/shotImageSM_' + (d.imageId) + '.jpeg")' : '';
                 });
+
+                grid.selectAll("div.act")
+                    .html(d => (
+                        '<table class="movie_table">' +
+                        '<col width="7%">' +
+                        '<col width="60%">' +
+                            makeTabRowData(d) +
+                            makeTabRowTitle(d.title) +
+                        '</table>')
+                    );
 
             });
 
